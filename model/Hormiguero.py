78d3512a45bef0e0f40f32e1f731d813e7416fa2
johansmartinez <johan.martinez.dev@gmail.com>
2023-05-28 01:00:07 -0500

    diagrama de poblacion segun momelo propuesto

--- model/Hormiguero.py
+++ model/Hormiguero.py
@@ -1,59 +1,76 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
 class Hormiguero:
-    def __init__(self,alto_hormiguero, ancho_hormiguero, poblacion_inicial, tasa_crecimiento, tasa_muerte, factor_generacion, esperanza_vida, comida_inicial, tamano_inicial, salud_reina):
+    def __init__(self,poblacion_inicial, dias_simulacion, vida_media_hormiga_reina, tamano_hormiguero, comida_dia, max_huevos_esp, taza_mortalidad, taza_enfermedad):
         self.poblacion = [poblacion_inicial]
-        self.alto=alto_hormiguero
-        self.ancho=ancho_hormiguero
-        self.tasa_crecimiento = tasa_crecimiento
-        self.tasa_muerte = tasa_muerte
-        self.factor_generacion = factor_generacion
-        self.esperanza_vida = esperanza_vida
-        self.comida = [comida_inicial]
-        self.tamano = [tamano_inicial]
-        self.salud_reina = [salud_reina]
-        self.fig_poblacion = plt.figure(figsize=(5, 3))
+        self.generacion_graf=[]
+        self.salud_reina_graf=[]
+        self.tasa_natalidad_graf=[]
+        self.nacimientos_graf=[]
+        self.dias_simulacion=dias_simulacion
+        self.tiempo=self.crear_vector(self.dias_simulacion)
+        self.vida_media_hormiga_reina=vida_media_hormiga_reina
+        self.tamano_hormiguero=tamano_hormiguero
+        self.comida_dia=comida_dia
+        self.max_huevos_esp=max_huevos_esp
+        self.taza_mortalidad=taza_mortalidad
+        self.taza_enfermedad=taza_enfermedad
+        self.calcular_poblacion()
+        
+    def crear_vector(self,numero):
+        vector = list(range(numero + 1))
+        return vector
+    
+    def calcular_poblacion(self):
+        for i, t in enumerate(self.tiempo):
+            #self.generacion_graf.append(self.generacion(t))
+            #self.salud_reina_graf.append(self.salud_hormiga_reina(t))
+            #self.tasa_natalidad_graf.append(self.tasa_natalidad(t))
+            #self.nacimientos_graf.append(self.nacimientos(t))
+            if i>0 and i <= len(self.poblacion):
+                poblacion_temp=self.nacimientos(t) - self.muertes( self.poblacion[(i-1)], t)
+                poblacion_temp+=self.poblacion[(i-1)]
+                self.poblacion.append(poblacion_temp)
+            
+                
+    def muertes(self, poblacion, tiempo):
+        return self.muertes_natural(tiempo)+self.enfermas_dia(poblacion, tiempo)
+    
+    def enfermas_dia(self, poblacion, tiempo):
+        return (poblacion/(self.generacion(tiempo)+1))*self.taza_enfermedad
+    
+    def muertes_natural(self,tiempo):
+        return self.nacimientos(tiempo)* self.taza_mortalidad
+        
+    def nacimientos(self, tiempo):
+        return self.tasa_natalidad(tiempo)
+    
+    def tasa_natalidad(self, tiempo):
+        sal_reina=self.salud_hormiga_reina(tiempo)
+        max_huev=self.max_huevos()
+        return (1-sal_reina)*self.min_huevos(max_huev)+sal_reina*max_huev
+    
+    def max_huevos(self):
+        return (self.comida_dia*self.max_huevos_esp)/self.tamano_hormiguero
+    
+    def min_huevos(self, max_huevos):
+        return max_huevos/6
+    
+    def salud_hormiga_reina(self, tiempo):
+        ope=(self.generacion(tiempo)+1)-(tiempo/self.vida_media_hormiga_reina)
+        if ope==0:
+            return 1
+        else:
+            return ope
+        
+    def generacion(self, tiempo):
+        return int(tiempo/self.vida_media_hormiga_reina)
+    
+#poblacion_inicial, dias_simulacion, vida_media_hormiga_reina, tamano_hormiguero, comida_dia, max_huevos_esp, taza_mortalidad, taza_enfermedad
+p=Hormiguero(10, 9300, 930, 7000, 300, 1400, 0.5, 0.01)
 
-    def simular_crecimiento(self, pasos):
-        for _ in range(pasos):
-            poblacion_actual = self.poblacion[-1]
-            nueva_cria = poblacion_actual * self.factor_generacion
-            nuevo_crecimiento = poblacion_actual * self.tasa_crecimiento
-            muertes = poblacion_actual * self.tasa_muerte
-
-            poblacion_nueva = poblacion_actual + nueva_cria + nuevo_crecimiento - muertes
-            self.poblacion.append(poblacion_nueva)
-
-            comida_actual = self.comida[-1]
-            comida_nueva = poblacion_nueva * 0.1  # Por ejemplo, se añade un 10% de la población como comida
-            comida_consumida = poblacion_actual * 0.05  # Por ejemplo, cada hormiga consume un 5% de comida
-            comida_disponible = comida_actual + comida_nueva - comida_consumida
-            self.comida.append(comida_disponible)
-
-            tamano_actual = self.tamano[-1]
-            tamano_nuevo = tamano_actual + (poblacion_nueva - poblacion_actual) * 0.01  # Por ejemplo, cada nueva hormiga aumenta el tamaño en un 1%
-            self.tamano.append(tamano_nuevo)
-
-            salud_reina_actual = self.salud_reina[-1]
-            if poblacion_nueva > 1000:  # Por ejemplo, si la población supera cierto umbral, se genera una nueva hormiga reina
-                salud_reina_nueva = salud_reina_actual - 0.1  # Por ejemplo, la salud de la nueva hormiga reina disminuye en 0.1
-                poblacion_nueva -= 1  # Restamos 1 a la población debido a la generación de la nueva hormiga reina
-            else:
-                salud_reina_nueva = salud_reina_actual
-            self.salud_reina.append(salud_reina_nueva)
-            self.poblacion[-1] = poblacion_nueva
-
-    def mostrar_grafico(self):
-        tiempo = np.arange(len(self.poblacion))
-        plt.plot(tiempo, self.poblacion, label='Población')
-        plt.plot(tiempo, self.comida, label='Comida')
-        plt.plot(tiempo, self.tamano, label='Tamaño')
-        plt.plot(tiempo, self.salud_reina, label='Salud de la Reina')
-        plt.xlabel('Tiempo')
-        plt.ylabel('Cantidad')
-        plt.title('Crecimiento del hormiguero')
-        plt.legend()
-        plt.show()
-
-
+plt.plot(p.tiempo, p.poblacion)
+plt.xlabel('dias')
+plt.ylabel('poblacion')
+plt.title('Poblacion')
+plt.show()
