f7d0930d998128dbeca028d75d5ea5ad5f43cbc5
AvellaLuisMiguel <avellaluismiguel@gmail.com>
2023-05-31 18:39:04 -0400

    Si

--- view/Menu.py
+++ view/Menu.py
@@ -4,7 +4,14 @@ from tkinter import ttk
 class Menu(ttk.Frame):
     def __init__(self,notebook):
         super().__init__(notebook)
-        self.boton_simulacion = tk.Button(self, text="Simulacion")
-        self.boton_simulacion.pack()
-        self.boton_modelo=tk.Button(self,text="Modelo")
-        self.boton_modelo.pack()
+        self.boton_simulacion = tk.Button(self, text="Simulacion",width=20,height=4)
+        self.boton_simulacion.place(x=600,y=75)
+        self.boton_modelo=tk.Button(self,text="Modelo",width=20,height=4)
+        self.boton_modelo.place(x=600,y=175)
+        self.proyecto=tk.Label(self,text="SIMULACION POR DINAMICA DE SISTEMAS\n DE UN HORMIGUERO CONTROLADO")
+        self.proyecto.place(x=80,y=40)
+        self.integrantes=tk.Label(self,text="INTEGRANTES:\n\nLuis Miguel Avella Avila\nJohan Sebastian Marniez Junco\nAndres Felipe Cañas Carrillo\nSantiago Moreno")
+        self.integrantes.place(x=110,y=100)
+
+
+    
